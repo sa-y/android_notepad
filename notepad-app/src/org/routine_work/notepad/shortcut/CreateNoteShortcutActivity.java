4d92045b9a2d9ff3ffff0d9b3e550c7cd9069537
Masahiko, SAWAI <say@users.sourceforge.jp>
2012-08-16 15:31:49 +0900

    ショートカット作成アクティビティを別パッケージに分離

--- notepad-app/src/org/routine_work/notepad/shortcut/CreateNoteShortcutActivity.java
+++ notepad-app/src/org/routine_work/notepad/shortcut/CreateNoteShortcutActivity.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.routine_work.notepad;
+package org.routine_work.notepad.shortcut;
 
 import android.app.Activity;
 import android.content.ContentResolver;
@@ -34,6 +34,8 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import org.routine_work.notepad.NotepadConstants;
+import org.routine_work.notepad.R;
 import org.routine_work.notepad.prefs.NotepadPreferenceUtils;
 import org.routine_work.notepad.provider.NoteStore;
 import org.routine_work.utils.Log;
