5e8a3760a42571c92385cea5d98a23cbced6e636
Masahiko, SAWAI <say@users.sourceforge.jp>
2014-10-02 06:39:37 +0900

    Changed database optimization timing

--- notepad-app/src/org/routine_work/notepad/NotepadActivity.java
+++ notepad-app/src/org/routine_work/notepad/NotepadActivity.java
@@ -221,7 +221,7 @@ public class NotepadActivity extends ListActivity
 		}
 
 		int quitCount = NotepadPreferenceUtils.incrementQuitCount(this);
-		if (quitCount % 10 == 0)
+		if (quitCount % 64 == 0)
 		{
 			Intent noteDBOptimizerIntent = new Intent(this, NoteDBOptimizer.class);
 			startService(noteDBOptimizerIntent);
