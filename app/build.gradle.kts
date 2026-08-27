ebb5a3e775e08596dc072b243611cad72d780288
Masahiko, SAWAI <masahiko.sawai@gmail.com>
2026-08-27 23:50:42 +0900

    Updated versionCode to 57

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "org.routine_work.notepad"
         minSdk = 24
         targetSdk = 37
-        versionCode = 56
+        versionCode = 57
         versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
